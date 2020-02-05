f4c6666be582de0d43ed057454ea6a581fe5f665
Jason Lengstorf <jason@lengstorf.com>
2020-02-05 15:47:13 -0800

    fix: fix relative paths

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const CACHE_DIRS = ['./.cache', './public'];
+const path = require('path');
+
+const getCacheDirs = constants => [
+  constants.BUILD_DIR,
+  path.normalize(`${constants.BUILD_DIR}/../.cache`),
+];
 
 module.exports = () => {
   return {
     name: 'netlify-plugin-gatsby-cache',
-    async onPreBuild({ utils }) {
-      if (await utils.cache.restore(CACHE_DIRS)) {
+    async onPreBuild({ constants, utils }) {
+      const cacheDirs = getCacheDirs(constants);
+
+      console.log({ cacheDirs });
+
+      if (await utils.cache.restore(cacheDirs)) {
         console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
       } else {
         console.log('No Gatsby cache found. Building fresh.');
       }
     },
-    async onPostBuild({ utils }) {
-      if (await utils.cache.save(CACHE_DIRS)) {
+    async onPostBuild({ constants, utils }) {
+      const cacheDirs = getCacheDirs(constants);
+
+      if (await utils.cache.save(cacheDirs)) {
         console.log('Stored the Gatsby cache to speed up future builds.');
       } else {
         console.log('Something went wrong storing the cache.');
