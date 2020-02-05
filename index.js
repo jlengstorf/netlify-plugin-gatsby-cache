ec44bc9b135d77d0456f47f14a0a6fb8ae5d1423
Jason Lengstorf <jason@lengstorf.com>
2020-02-05 12:43:31 -0800

    fix: I am a doofus

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
+const path = requrie('path');
+
 module.exports = () => {
   return {
     name: 'netlify-plugin-gatsby-cache',
     async onPreBuild({ netlifyConfig, utils }) {
-      console.log([
-        `${netlifyConfig.build.publish}/.cache`,
-        `${netlifyConfig.build.publish}/public`,
-      ]);
+      const cacheDirectories = [
+        path.join(netlifyConfig.build.publish, '..', '.cache'),
+        netlifyConfig.build.publish,
+      ];
 
-      const hasCacheRestored = await utils.cache.restore([
-        `${netlifyConfig.build.publish}/.cache`,
-        `${netlifyConfig.build.publish}/public`,
-      ]);
+      console.log({ cacheDirectories });
 
-      if (await utils.cache.has(`${netlifyConfig.build.publish}/.cache`)) {
-        console.log('cache dir found!');
-      } else {
-        console.log('no cache dir :(');
-      }
-
-      console.log({ hasCacheRestored });
-
-      if (hasCacheRestored) {
+      if (await utils.cache.restore(cacheDirectories)) {
         console.log(
           'Loaded a previous Gatsby cache. Buckle up; we’re about to go FAST. ⚡️',
         );
+      } else {
+        console.log('No Gatsby cache found. Building fresh.');
       }
     },
     async onPostBuild({ netlifyConfig, utils }) {
-      console.log([
-        `${netlifyConfig.build.publish}/.cache`,
-        `${netlifyConfig.build.publish}/public`,
-      ]);
+      const cacheDirectories = [
+        path.join(netlifyConfig.build.publish, '..', '.cache'),
+        netlifyConfig.build.publish,
+      ];
 
-      const hasCacheSaved = await utils.cache.save([
-        `${netlifyConfig.build.publish}/.cache`,
-        `${netlifyConfig.build.publish}/public`,
-      ]);
-
-      console.log({ hasCacheSaved });
-
-      if (await utils.cache.has(`${netlifyConfig.build.publish}/.cache`)) {
-        console.log('cache dir found!');
-      } else {
-        console.log('no cache dir :(');
-      }
-
-      if (hasCacheSaved) {
+      if (await utils.cache.save(cacheDirectories)) {
         console.log('Stored the Gatsby cache to speed up future builds.');
+      } else {
+        console.log('Something went wrong storing the cache.');
       }
     },
   };
