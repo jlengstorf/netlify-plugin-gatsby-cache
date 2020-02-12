25c318a2fdef0c01e2a90d6b643fc118d85b3ff2
Jason Lengstorf <jason@lengstorf.com>
2020-02-12 10:02:08 -0800

    feat: switch to new cache utils (#3)
    * feat: switch to cache utils
    
    * chore: bump version
    
    * fix: use the right config object
    
    * chore: bump package version
    
    * fix: use the proper lifecycle and add temporary logging
    
    * fix: add temporary logging
    
    * chore: bump package version
    
    * fix: use cache.has
    
    * fix: I am a doofus
    
    * fix: I am *really* a doofus
    
    * chore: add logging (#4)
    
    * chore: more logging
    
    * fix: it’s all relative
    
    * fix: fix relative paths
    
    * chore: remove unused deps
    
    * fix: remove extra logging
    
    Co-authored-by: David Wells <hello@davidwells.io>

--- index.js
+++ index.js
@@ -1,66 +1,30 @@
-const fs = require("fs-extra");
-const path = require("path");
+const path = require('path');
 
-const getNetlifyCacheDirs = ({ config, constants }) => {
-  const cacheDir = constants.CACHE_DIR;
-  const gatsbyDir = path.dirname(config.build.publish);
-
-  return {
-    gatsbyCacheDir: path.join(gatsbyDir, ".cache"),
-    gatsbyPublicDir: path.join(gatsbyDir, "public"),
-    netlifyCacheDir: path.join(cacheDir, "gatsby/.cache"),
-    netlifyPublicDir: path.join(cacheDir, "gatsby/public")
-  };
-};
+const getCacheDirs = constants => [
+  constants.BUILD_DIR,
+  path.normalize(`${constants.BUILD_DIR}/../.cache`),
+];
 
 module.exports = () => {
   return {
-    name: "netlify-plugin-gatsby-cache",
-    onPreBuild: async args => {
-      const {
-        gatsbyCacheDir,
-        gatsbyPublicDir,
-        netlifyCacheDir,
-        netlifyPublicDir
-      } = getNetlifyCacheDirs(args);
+    name: 'netlify-plugin-gatsby-cache',
+    async onPreBuild({ constants, utils }) {
+      const cacheDirs = getCacheDirs(constants);
 
-      if (!fs.existsSync(netlifyCacheDir) || !fs.existsSync(netlifyPublicDir)) {
-        console.log("No Gatsby cache found. Building fresh...");
-        return;
+      if (await utils.cache.restore(cacheDirs)) {
+        console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
+      } else {
+        console.log('No Gatsby cache found. Building fresh.');
       }
-
-      await Promise.all([
-        fs.copy(netlifyCacheDir, gatsbyCacheDir),
-        fs.copy(netlifyPublicDir, gatsbyPublicDir)
-      ])
-        .then(() => {
-          console.log(
-            "Loaded a previous Gatsby cache. Buckle up; we’re about to go FAST. ⚡️"
-          );
-        })
-
-        .catch(() => {
-          console.error(error.message);
-        });
     },
-    onSaveCache: async args => {
-      const {
-        gatsbyCacheDir,
-        gatsbyPublicDir,
-        netlifyCacheDir,
-        netlifyPublicDir
-      } = getNetlifyCacheDirs(args);
+    async onPostBuild({ constants, utils }) {
+      const cacheDirs = getCacheDirs(constants);
 
-      await Promise.all([
-        fs.copy(gatsbyCacheDir, netlifyCacheDir),
-        fs.copy(gatsbyPublicDir, netlifyPublicDir)
-      ])
-        .then(() => {
-          console.log("Stored the Gatsby cache to speed up future builds.");
-        })
-        .catch(error => {
-          console.error(error.message);
-        });
-    }
+      if (await utils.cache.save(cacheDirs)) {
+        console.log('Stored the Gatsby cache to speed up future builds.');
+      } else {
+        console.log('Something went wrong storing the cache.');
+      }
+    },
   };
 };
