6912c77fa5e5a80e846155746d6c9bfeeb75302b
ehmicky <ehmicky@gmail.com>
2020-03-31 21:22:08 +0200

    Use new Netlify API

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const path = require('path');
 
 const getCacheDirs = constants => [
-  constants.BUILD_DIR,
-  path.normalize(`${constants.BUILD_DIR}/../.cache`),
+  constants.PUBLISH_DIR,
+  path.normalize(`${constants.PUBLISH_DIR}/../.cache`),
 ];
 
-module.exports = () => {
-  return {
-    name: 'netlify-plugin-gatsby-cache',
-    async onPreBuild({ constants, utils }) {
-      const cacheDirs = getCacheDirs(constants);
+module.exports = {
+  async onPreBuild({ constants, utils }) {
+    const cacheDirs = getCacheDirs(constants);
 
-      if (await utils.cache.restore(cacheDirs)) {
-        console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
-      } else {
-        console.log('No Gatsby cache found. Building fresh.');
-      }
-    },
-    async onPostBuild({ constants, utils }) {
-      const cacheDirs = getCacheDirs(constants);
+    if (await utils.cache.restore(cacheDirs)) {
+      console.log('Found a Gatsby cache. We’re about to go FAST. ⚡️');
+    } else {
+      console.log('No Gatsby cache found. Building fresh.');
+    }
+  },
+  async onPostBuild({ constants, utils }) {
+    const cacheDirs = getCacheDirs(constants);
 
-      if (await utils.cache.save(cacheDirs)) {
-        console.log('Stored the Gatsby cache to speed up future builds.');
-      } else {
-        console.log('Something went wrong storing the cache.');
-      }
-    },
-  };
+    if (await utils.cache.save(cacheDirs)) {
+      console.log('Stored the Gatsby cache to speed up future builds.');
+    } else {
+      console.log('Something went wrong storing the cache.');
+    }
+  },
 };
