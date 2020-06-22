da8c322b267f966c665d226a5f2c015d4364252d
Jason Lengstorf <jason@lengstorf.com>
2020-06-22 11:36:38 -0700

    fix: check for a publish directory (#34)

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const path = require('path');
 
-const getCacheDirs = constants => [
+const getCacheDirs = (constants) => [
   constants.PUBLISH_DIR,
   path.normalize(`${constants.PUBLISH_DIR}/../.cache`),
 ];
 
 module.exports = {
   async onPreBuild({ constants, utils }) {
+    // print a helpful message if the publish dir is misconfigured
+    if (process.cwd() === constants.PUBLISH_DIR) {
+      utils.build.failBuild(
+        `Gatsby sites must publish the public directory, but your site’s publish directory is set to “${constants.PUBLISH_DIR}”. Please set your publish directory to your Gatsby site’s public directory.`,
+      );
+    }
+
     const cacheDirs = getCacheDirs(constants);
 
     if (await utils.cache.restore(cacheDirs)) {
